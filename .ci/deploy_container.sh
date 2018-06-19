1112f2943e81fe381831aa03793574f4e9af5204
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2018-06-19 09:23:26 +0200

    Add optional prefix to deployed container name
    * if CONTAINER_PREFIX == "gerrit" also append the changeset number
    
    Change-Id: I59ae3d865fb3967b2dce194458de367f6573265f

--- .ci/deploy_container.sh
+++ .ci/deploy_container.sh
@@ -6,11 +6,21 @@ INSTALL_DIR=/containers/jenkins
 IMAGE_NAME=singularity_spack_${SPACK_BRANCH}.img
 DATE=$(date --iso)
 
+get_container_name()
+{
+    local local_num="$1"
+    local cprefix="${CONTAINER_PREFIX}"
+    if [ "${cprefix}" = "gerrit" ]; then
+        cprefix="${CONTAINER_PREFIX}_cs_${GERRIT_CHANGE_NUMBER}"
+    fi
+    echo -n "$INSTALL_DIR/singularity_spack_${cprefix:+${cprefix}_}${SPACK_BRANCH}-${DATE}-${local_num}.img"
+}
+
 # find unused image name
 num=1
-while [[ -e "$INSTALL_DIR/singularity_spack_${SPACK_BRANCH}-${DATE}-${num}.img" ]] ; do
+while [[ -e "$(get_container_name ${num})" ]] ; do
     let num++
 done
 
 # copy to target
-cp -v ${IMAGE_NAME} "$INSTALL_DIR/singularity_spack_${SPACK_BRANCH}-${DATE}-${num}.img"
+cp -v "${IMAGE_NAME}" "$(get_container_name ${num})"
