0d629f257a5bb4514d0ee8b8cc5fbd31bc4d6f25
Eric Müller <mueller@kip.uni-heidelberg.de>
2018-07-03 13:20:14 +0200

    Fixup "Save more space when building"
    Change-Id: I9da5779798ce450ba2ea8ffefad90f82d7a6de20

--- .ci/remove_ccache.sh
+++ .ci/remove_ccache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
 # Do not change: special sudo permit for jenkins user...
-sudo rm -rf ccache/ || exit 0
+sudo /bin/rm -rf ccache/ || exit 0
--- .ci/remove_tmp_spack.sh
+++ .ci/remove_tmp_spack.sh
@@ -2,5 +2,7 @@
 
 # Do not change: special sudo permit for jenkins user...
 for tempfolder in /tmp/spack/tmp.*; do
-    sudo rm -rf ${tempfolder} || exit 0
+    if [ -d ${tempfolder} ]; then
+        sudo /bin/rm -rf ${tempfolder} || exit 0
+    fi
 done
