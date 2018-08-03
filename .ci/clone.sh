49613943710e27d23bbd043a692613f39c2ee55e
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2018-08-03 15:08:33 +0200

    Add support for Depends-On in COMMITMSG
    * Change from spack repository will be checked out
    
    Depends-On: 4440
    
    Change-Id: Iefa9d658829e9a43ade433c3ee2084ebf2a2379e

--- .ci/clone.sh
+++ .ci/clone.sh
@@ -5,21 +5,71 @@ if [ -z "${SPACK_BRANCH}" ]; then
     exit 1
 fi
 
-if [ -z "${MY_GERRIT_BASE_URL}" ]; then
-    export MY_GERRIT_BASE_URL="ssh://hudson@brainscales-r.kip.uni-heidelberg.de:29418/"
+if [ -z "${GERRIT_USERNAME}" ]; then
+    GERRIT_USERNAME="hudson"
+fi
+
+if [ -z "${GERRIT_PORT}" ]; then
+    GERRIT_PORT=29418
+fi
+
+if [ -z "${GERRIT_HOSTNAME}" ]; then
+    GERRIT_HOSTNAME="brainscales-r.kip.uni-heidelberg.de"
+fi
+
+
+if [ -z "${GERRIT_BASE_URL}" ]; then
+    export GERRIT_BASE_URL="ssh://${GERRIT_USERNAME}@${GERRIT_HOSTNAME}:${GERRIT_PORT}"
 fi
 
 # clone spack installation ouside and copy into the container
-MY_GERRIT_URL="${MY_GERRIT_BASE_URL}/spack"
+MY_GERRIT_URL="${GERRIT_BASE_URL}/spack"
 rm -rf spack_${SPACK_BRANCH}
 git clone ${MY_GERRIT_URL} -b ${SPACK_BRANCH} spack_${SPACK_BRANCH}
 
-# TODO: also watch for spack changes...
+# Order of importance:
+# 1. jenkins-specified SPACK_GERRIT_REFSPEC
+# 2. jenkins-specified SPACK_GERRIT_CHANGE
+# 3. commit-specified  Depends-On
+#
+# If multiple are specified, take the first variable defined according the
+# order above.
+
+if [ -z "${SPACK_GERRIT_CHANGE}" ] && [ -z "${SPACK_GERRIT_REFSPEC}" ]; then
+    # see if the commit message contains a "Depends-On: xy" line
+    # if there are several lines, concatenate with commas
+    SPACK_GERRIT_CHANGE=$(git log -1 --pretty=%B \
+        | awk '$1 ~ "Depends-On:" { print $2 }' | tr '\n' ',' | tr -d \[:space:\])
+else
+    echo "SPACK_GERRIT_CHANGE or SPACK_GERRIT_REFSPEC specified, ignoring "\
+         "possible 'Depends-On'-line in commit message!"
+fi
+
+# if there is a spack gerrit change specified and no refspec -> resolve!
+if [ -n "${SPACK_GERRIT_CHANGE}" ] && [ -z "${SPACK_GERRIT_REFSPEC}" ]; then
+    # convert spack change id to latest patchset
+    pushd spack_${SPACK_BRANCH}
+
+    for change in ${SPACK_GERRIT_CHANGE//,/ }; do
+        SPACK_GERRIT_REFSPEC=$(ssh -p ${GERRIT_PORT} \
+                ${GERRIT_USERNAME}@${GERRIT_HOSTNAME} gerrit query \
+                --current-patch-set ${change} \
+            | grep "^[[:space:]]*ref:" | cut -d : -f 2 | tr -d \[:space:\])
+
+        # break as soon as we have the change for the spack repo
+        if [ -n "${SPACK_GERRIT_REFSPEC}" ]; then
+            break
+        fi
+    done
+
+    popd
+fi
+
 if [ -n "${SPACK_GERRIT_REFSPEC}" ]; then
     echo "SPACK_GERRIT_REFSPEC was specified: ${SPACK_GERRIT_REFSPEC} -> checking out"
-    cd spack_${SPACK_BRANCH}
+    pushd spack_${SPACK_BRANCH}
     git fetch  ${MY_GERRIT_URL} ${SPACK_GERRIT_REFSPEC} && git checkout FETCH_HEAD
-    cd ..
+    popd
 fi
 
 # set download mirror stuff to prefill outside of container
