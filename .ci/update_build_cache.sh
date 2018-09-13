67296eef2fe15f0773f45b716a4840fa5c3f83b3
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2018-09-13 12:06:10 +0200

    Fix build cache not updating
    Change-Id: Ie2a758bf72dcc73f46e6b1e7901d207c1b79209b

--- .ci/update_build_cache.sh
+++ .ci/update_build_cache.sh
@@ -2,7 +2,7 @@
 
 # taken from ./deploy_container.sh -> TODO: move all variables to single file!
 IMAGE_NAME=singularity_spack_temp.img
-BUILD_CACHE_DIR=${WORKSPACE}/build_cache
+BUILD_CACHE_DIR=${HOME}/build_cache
 
 # do not fail the build if some updates in the build-cache fail
 singularity exec -B "${BUILD_CACHE_DIR}:/opt/build_cache:rw" "${IMAGE_NAME}" "${WORKSPACE}/misc-files/update-build-cache-in-container.sh" || exit 0
