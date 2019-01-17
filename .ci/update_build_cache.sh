41c2544fbd9179e5c64678bfbe461f8ca908f1fd
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-01-17 15:45:41 +0100

    Only update buildcache in stable builds
    Change-Id: I7617f0fdcb40f169a9f35fd74fc28f8573cab4f2

--- .ci/update_build_cache.sh
+++ .ci/update_build_cache.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -x
 
+# only update build cache for stable builds
+if [ "${CONTAINER_BUILD_TYPE}" != "stable" ]; then
+    echo "Not updating build cache for testing builds." 1>&2
+    exit 0
+fi
+
 # taken from ./deploy_container.sh -> TODO: move all variables to single file!
 IMAGE_NAME=singularity_spack_temp.img
 BUILD_CACHE_DIR=${HOME}/build_cache
