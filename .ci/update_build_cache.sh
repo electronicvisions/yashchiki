b72dcca8a55b1efecd43ade86c23d5eb91d43d05
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-01-24 15:42:35 +0100

    Fix update_build_cache.sh
    Change-Id: Ifabc4c6d3499d7e9d490c5078272533f63722a85

--- .ci/update_build_cache.sh
+++ .ci/update_build_cache.sh
@@ -11,4 +11,8 @@ IMAGE_NAME=singularity_spack_temp.img
 BUILD_CACHE_DIR=${HOME}/build_cache
 
 # do not fail the build if some updates in the build-cache fail
-sudo -u spack singularity exec -B "${BUILD_CACHE_DIR}:/opt/build_cache:rw" "${IMAGE_NAME}" "${WORKSPACE}/misc-files/update-build-cache-in-container.sh" || exit 0
+# since the spack user cannot read home of vis_jenkins we need to mount the update script inside the container
+sudo -u spack singularity exec\
+    -B "${BUILD_CACHE_DIR}:/opt/build_cache:rw"\
+    -B "${WORKSPACE}/misc-files/update-build-cache-in-container.sh:/update.sh"\
+    "${IMAGE_NAME}" /update.sh || exit 0
