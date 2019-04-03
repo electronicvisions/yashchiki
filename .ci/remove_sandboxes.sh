164aeb238c81af35e95917aac0c3dd9c8d72153b
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-04-03 14:14:12 +0200

    Move cleanup into Jenkinsfile / add checkout
    Because at the beginning of the job we only have access to the
    Jenkinsfile, cleanup has to be performed by the Jenkinsfile itself.
    
    Change-Id: Id69401994251987ee83d2ae385996f0457304b2f

--- .ci/remove_sandboxes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-
-# Do not change: special sudo permit for jenkins user...
-sudo /bin/rm -rf sandboxes/ || exit 0
--- .ci/remove_tmp_spack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-set -euo pipefail
-
-# NOTE: Do not allow more than one build per executor!
-# Do not change: special sudo permit for jenkins user...
-sudo /bin/rm -rf "/tmp/${NODE_NAME}/"
