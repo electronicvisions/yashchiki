044a1af0436c6a7f2658f848e6b360becc5d7eb7
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-10-30 11:46:28 +0100

    Include path to cache in case of build error
    When a container build fails, all packages that were successfully built
    up to that point are stored in a fail-cache.
    
    If the same gerrit change gets built again, that cache is used
    automatically to reduce turn-around.
    
    Sometimes, work shifts from one gerrit change to another, but the user
    still wishes to preserve his build-progress.
    
    Hence, this change makes it explicit which cache was created in case of
    build errors.
    
    Change-Id: I6842ba256cc94e5e3c92ff772c8e3de10d380822

--- .ci/create_temporary_build_cache_after_failure.sh
+++ .ci/create_temporary_build_cache_after_failure.sh
@@ -5,6 +5,8 @@
 #
 # It streamlines container deployment as every container deployment will
 # restart at the last sucessfully built package.
+#
+# Echoes path to created cache relative to build_caches base directory.
 
 if [ "${CONTAINER_BUILD_TYPE}" = "stable" ]; then
     echo "Stable container creation failed, this should not happen." >&2
@@ -20,6 +22,11 @@ change_num="$(get_change_name)"
 while [ -d "${PRESERVED_PACKAGES_OUTSIDE}/${change_num}_${build_num}" ]; do
     (( build_num++ ))
 done
+# `build_num` is used to indicate if no preserved packages were found: After
+# the loop, `build_num` will be one higher than the last folder with
+# preserved_packages found. Hence, if no folder was found, it will still be 1
+# -> setting it to 0 encodes that we found no preserved_packages that we should
+# store in the failed cache.
 (( build_num-- ))
 
 preserved_packages="${PRESERVED_PACKAGES_OUTSIDE}/${change_num}_${build_num}"
@@ -28,7 +35,7 @@ failed_build_cache="${BASE_BUILD_CACHE_FAILED_OUTSIDE}/${change_num}_${build_num
 # expects input to be \0-printed
 link_into_failed_buildcache() {
     xargs -0 -r realpath "--relative-to=${failed_build_cache}" \
-    | xargs -r ln -sv -t "${failed_build_cache}"
+    | xargs -r ln -sv -t "${failed_build_cache}" >&2
 }
 
 # ensure that we have preserved packages and that these packages have not
@@ -36,7 +43,7 @@ link_into_failed_buildcache() {
 if (( build_num > 0 )) && \
     [ ! -d "${BASE_BUILD_CACHE_FAILED_OUTSIDE}/${change_num}_${build_num}" ]; then
 
-    mkdir -p "${failed_build_cache}"
+    mkdir -vp "${failed_build_cache}" >&2
 
     # link all newly preserved packages (relatively so links work in and
     # outside of containers)
@@ -44,4 +51,9 @@ if (( build_num > 0 )) && \
 
     # link everything not present in preserved packages that is in build cache
     find "${BUILD_CACHE_OUTSIDE}" -name "*.tar.gz" -print0 | link_into_failed_buildcache
+
+    # echo created failed buildcache
+    echo "${failed_build_cache#${BASE_BUILD_CACHE_OUTSIDE}}"
+else
+    echo "<no preserved packages found>"
 fi
