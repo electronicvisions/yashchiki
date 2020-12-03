4ecdddd3bea8ea032732d4699f197301d91d4d11
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-12-03 16:18:18 +0100

    Archive concretized specs for all builds
    Problem encountered along the way:
    * The Jenkinsfile had no "proper" way of accessing the environment
      information validated and stored in disk in in
      `.ci/validate_environment.sh`.
    
    Solution:
    * Provide `.ci/get_jenkins_env.sh` helper script that simply executes
      the function from `commons.sh` of the same name.
    
    Change-Id: I454243595ae5bda288d945cec32b591a0f0d997d

---
+++ .ci/get_jenkins_env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Usage:
+#   get_jenkins_env.sh <variable-name>
+#
+# Stand-alone shell variant to the corresponding function from commons.sh to
+# allow retrieval of environment information in Jenkinsfile.
+#
+# Get <variable-name> from the jenkins environment dumped at the start of the
+# jenkins job.  If the jenkins environment was not dumped at the beginning of
+# the jenkins job, the regular environment is taken.
+#
+# If the variable is not found, return 1.
+
+if (( $# > 1 || $# == 0 )); then
+    echo "ERROR: $0 expects exactly one argument!" >&2
+    exit 1
+fi
+
+SOURCE_DIR="$(dirname "$(readlink -m "${BASH_SOURCE[0]}")")"
+source "${SOURCE_DIR}/commons.sh"
+
+get_jenkins_env "$1"
