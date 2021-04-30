d43d5fec07e4be02b96bebe148e3c10224e3941d
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2021-04-30 22:50:07 +0200

    add script for pinning extraction
    Change-Id: Ia3bf5eb342404753a28c0551794a3e6d3b0de3e3

---
+++ utils/extract_current_pinnings.py
@@ -0,0 +1,25 @@
+#!/usr/env/python
+
+''''Util to extract a list of pinnings from the current container'''
+
+import yaml
+import argparse
+
+
+apps = ['wafer', 'dls', 'spikey', 'simulation', 'slurmviz']
+
+
+for app in apps:
+    speclist = ''
+    try:
+        spec = yaml.load(open(f'/opt/spack_views/visionary-{app}/.spack/visionary-{app}/spec.yaml'))
+        for singlespec in spec['spec'][1:]:
+            for k, v in singlespec.items():
+                speclist += f'{k}@{v["version"]}\n'
+
+        print(f'Writing dependency list "{spec-dep-{app}.list}"')
+        with open(f'spec-dep-{app}.list', 'w') as f:
+            f.write(speclist)
+    except:
+        print('Script must be run from within a container.')
+        raise
