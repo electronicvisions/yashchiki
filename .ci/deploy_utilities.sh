91e898c1f6cf8db0efc07e2e94e6cc38f603ac2f
Eric Müller <mueller@kip.uni-heidelberg.de>
2022-09-06 12:05:40 +0200

    Fixup 139a84 (fixes deploy_utilities.sh)
    Change-Id: Ie455e79979307d93921609bba74a11cc50b25568

--- .ci/deploy_utilities.sh
+++ .ci/deploy_utilities.sh
@@ -8,5 +8,5 @@ NAME_FILTER=( "(" -name "*.py" -or -name "*.sh" ")" )
 if [ "${CONTAINER_BUILD_TYPE}" = "stable" ]; then
     find "${TARGET_FOLDER}" "${NAME_FILTER[@]}" -delete
     find "${SOURCE_FOLDER}" "${NAME_FILTER[@]}" -print0 \
-        | xargs -n 1 -0 "$(git rev-parse --show-toplevel)/.ci/deploy_utility_with_preamble.sh" "${TARGET_FOLDER}"
+        | xargs -n 1 -0 "${PWD}/yashchiki/.ci/deploy_utility_with_preamble.sh" "${TARGET_FOLDER}"
 fi
