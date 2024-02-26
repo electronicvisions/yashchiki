cf2d91ddf3fbe422edfe37e203a054f307218db0
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2024-02-26 11:07:33 +0100

    fix: deploy utilities
    Change-Id: I26745af828b8714aa95aa50a52ba1567fa1abd59

--- bin/yashchiki_deploy_utilities.sh
+++ bin/yashchiki_deploy_utilities.sh
@@ -9,5 +9,5 @@ NAME_FILTER=( "(" -name "*.py" -or -name "*.sh" ")" )
 if [ "${CONTAINER_BUILD_TYPE}" = "stable" ]; then
     find "${TARGET_FOLDER}" "${NAME_FILTER[@]}" -delete
     find "${SOURCE_FOLDER}" "${NAME_FILTER[@]}" -print0 \
-        | xargs -n 1 -0 "${sourcedir}/../lib/yashchiki/deploy_utility_with_preamble.sh" "${TARGET_FOLDER}"
+        | xargs -n 1 -0 "${ROOT_DIR}/lib/yashchiki/deploy_utility_with_preamble.sh" "${TARGET_FOLDER}"
 fi
