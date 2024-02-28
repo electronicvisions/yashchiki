284fdc783bb8c121f941e37c394a7a72224a458a
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2024-02-28 14:26:54 +0100

    chore: Remove unused dummy_variables.sh
    Change-Id: Iaeeb019697f01a334bb1cd2fcf5e2e712ce006fe

--- lib/yashchiki/dummy_variables.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Setting undefined required environment variables to 'undefined'." >&2
-
-export BUILD_CACHE_NAME="${BUILD_CACHE_NAME:-undefined}"
-export DEPENDENCY_PYTHON="${DEPENDENCY_PYTHON:-undefined}"
-export YASHCHIKI_SPACK_GCC="${YASHCHIKI_SPACK_GCC:-undefined}"
-export WORKSPACE="${WORKSPACE:-undefined}"
