a642a60604fbf8498342858d72216c08724d76b5
Jakob Kaiser <jakob.kaiser@kip.uni-heidelberg.de>
2024-06-25 10:50:29 +0000

    chore: make naming of file more clear
    Change-Id: I15d3972a48476cc7ede3a9369c684ae35cc620f5

--- lib/yashchiki/complete_spack_install_routine_called_in_post_as_root.sh
+++ lib/yashchiki/complete_spack_install_routine_called_in_post_as_root.sh
@@ -14,7 +14,7 @@ source "${SOURCE_DIR}/commons.sh"
 "${SPACK_INSTALL_SCRIPTS}/prepare_spack_as_root.sh"
 sudo -Eu spack "${SPACK_INSTALL_SCRIPTS}/install_dependencies.sh"
 
-sudo -Eu spack "${SPACK_INSTALL_SCRIPTS}/install_spack.sh" && \
+sudo -Eu spack "${SPACK_INSTALL_SCRIPTS}/install_spack_packages.sh" && \
 sudo -Eu spack ${MY_SPACK_CMD} compiler add --scope site /usr/bin && \
 sudo -Eu spack "${SPACK_INSTALL_SCRIPTS}/restore_spack_user_settings.sh" && \
 # remove temporary cache folder
