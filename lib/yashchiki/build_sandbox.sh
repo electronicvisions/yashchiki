1176ec693a8f559e64dae9abf0d017c7c720e244
Eric Müller <mueller@kip.uni-heidelberg.de>
2024-07-22 17:16:42 +0200

    fix: skip build_sandbox if stage not specified
    Change-Id: I68dbe2c71981c80d2498a8e9fa0d31f8452d7dca

--- lib/yashchiki/build_sandbox.sh
+++ lib/yashchiki/build_sandbox.sh
@@ -3,6 +3,11 @@
 set -euo pipefail
 shopt -s inherit_errexit 2>/dev/null || true
 
+if [ -z "${YASHCHIKI_ENABLE_STAGE_BUILD_BASE:-}" ]; then
+    echo "Skipping stage build-base."
+    exit 0
+fi
+
 # set generic locale for building
 export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
