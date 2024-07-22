79a8af2a7a3a41b685ee3dee13c121bf33bf060b
Eric Müller <mueller@kip.uni-heidelberg.de>
2024-07-22 18:44:42 +0200

    fix: skip build_image if stage not specified
    Change-Id: I0c9696bb83e7c13fd86cfa7d7c818c23853079d2

--- lib/yashchiki/build_image.sh
+++ lib/yashchiki/build_image.sh
@@ -3,6 +3,11 @@
 set -euo pipefail
 shopt -s inherit_errexit 2>/dev/null || true
 
+if [ -z "${YASHCHIKI_ENABLE_STAGE_IMAGE:-}" ]; then
+    echo "Skipping stage build-base-sandbox."
+    exit 0
+fi
+
 TARGET_FOLDER="${YASHCHIKI_SANDBOXES}/${CONTAINER_STYLE}"
 
 if test -f "${YASHCHIKI_IMAGE_NAME}"; then
