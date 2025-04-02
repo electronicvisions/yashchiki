e179e074c340d3aa9c9a0dacddafd3e6b20b3d02
Jakob Kaiser <jakob.kaiser@kip.uni-heidelberg.de>
2025-04-02 09:55:35 +0000

    feat: preserve packages in case `spack install` fails
    Change-Id: I95b506f7e97c5bf786050ffa2b7a576d69b3b028

--- lib/yashchiki/complete_spack_install_routine_called_in_post.sh
+++ lib/yashchiki/complete_spack_install_routine_called_in_post.sh
@@ -12,16 +12,15 @@ SOURCE_DIR="$(dirname "$(readlink -m "${BASH_SOURCE[0]}")")"
 source "${SOURCE_DIR}/commons.sh"
 
 "${SPACK_INSTALL_SCRIPTS}/prepare_spack.sh"
-"${SPACK_INSTALL_SCRIPTS}/install_dependencies.sh"
 
-"${SPACK_INSTALL_SCRIPTS}/install_spack_packages.sh" && \
-${MY_SPACK_CMD} compiler add --scope site /usr/bin && \
-"${SPACK_INSTALL_SCRIPTS}/restore_spack_user_settings.sh" && \
+trap "${SPACK_INSTALL_SCRIPTS}/preserve_built_spack_packages.sh" ERR
+"${SPACK_INSTALL_SCRIPTS}/install_dependencies.sh"
+"${SPACK_INSTALL_SCRIPTS}/install_spack_packages.sh"
+
+${MY_SPACK_CMD} compiler add --scope site /usr/bin
+
+"${SPACK_INSTALL_SCRIPTS}/restore_spack_user_settings.sh"
 # remove temporary cache folder
 rm -rfv /opt/spack/.spack
 
-"${SPACK_INSTALL_SCRIPTS}/generate_modules.sh" || \
-(
-    "${SPACK_INSTALL_SCRIPTS}/preserve_built_spack_packages.sh" &&
-    exit 1  # propagate the error
-)
+"${SPACK_INSTALL_SCRIPTS}/generate_modules.sh"
